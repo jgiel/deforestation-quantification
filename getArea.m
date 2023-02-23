11dfb17d25d42a73e1a84fe84ad553f34dd137aa
josh.senvo <joshsenvo@gmail.com>
2023-02-23 13:25:43 -0700

    add area, mask, live file

---
+++ getArea.m
@@ -0,0 +1,17 @@
+function results = getArea(I)
+%takes image and returns area in km^2 (from Matlab)
+
+% Segment
+BW = createMask(I);
+
+% Area in pixels
+props = regionprops("table",BW,"Area");
+areaPixels = sum(props.Area);
+
+% Area in km^2
+px2km = (51/20)^2;
+areaKmSq = round(areaPixels*px2km);
+
+% Format into structure
+results.BW = BW;
+results.areaKmSq = areaKmSq;
